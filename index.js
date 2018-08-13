02cdbab7cc3634f428976a7142ae82caf11f94d4
Intisaar <xakiin6@hotmail.com>
2018-08-13 20:49:49 +0100

    buil

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 80
+
+express()
+  .use(express.static(path.join(__dirname, '/')))
+  
+.get('/', (req, res) => res.render('index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
